ef7312222f44127a4ca95e9e56c3f4f6ff6add0e
realhwi <wlsgnl49@gmail.com>
2024-10-07 22:31:59 +0900

    02_퍼즐화면 배치 및 좌표변환

--- Match3/proj.win32/main.cpp
+++ Match3/proj.win32/main.cpp
@@ -22,6 +22,7 @@
  THE SOFTWARE.
  ****************************************************************************/
 
+#include "stdafx.h"
 #include "main.h"
 #include "AppDelegate.h"
 #include "cocos2d.h"
